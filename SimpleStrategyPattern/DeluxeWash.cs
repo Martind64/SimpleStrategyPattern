98bb296e1c1a4bdaadc80681d65adc5ad54cc25b
Martind64 <Martind_64@hotmail.com>
2014-12-11 16:11:39 +0100

    SimpleStrategyPattern CarWash

---
+++ SimpleStrategyPattern/DeluxeWash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStrategyPattern
+{
+    public class DeluxeWash : ICarWashStrategy
+    {
+
+        // Inherit from the ICarWashStrategy interface and make the CarWash method for DeluxecWash that implements the properties of DeluxeWash
+        public void CarWash(Car car)
+        {
+            Console.WriteLine("** Deluxe Wash initiated on {0} **", car._name);
+            Console.WriteLine("Add lots of Water");
+            Console.WriteLine("Spray plenty of Soap");
+            Console.WriteLine("Tire Brush");
+            Console.WriteLine("Wrap-Around Washers");
+            Console.WriteLine("Rinse and Wax");
+            Console.WriteLine("Soft Dry");
+            Console.WriteLine("** Deluxe Wash Completed **");
+        }
+    }
+}
